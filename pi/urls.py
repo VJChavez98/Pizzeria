2afb376f637afba1d0e34018593a1914f1c41312
zodiac98 <chavez98victor@gmail.com>
2019-06-22 22:30:31 -0600

    1.1.1 commit en prueba1 con login funcionando al cien

---
+++ pi/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path
+from . import views
+
+#django login, del in case of err
+from django.conf.urls import url
+from django.http import HttpResponseRedirect
+from django.views.generic.base import TemplateView
+from django.urls import include, reverse
+
+
+app_name = 'pi'
+
+urlpatterns=[
+    #path(' ',views.index,name = 'index'),
+    #esto es por defecto pi/
+    path('', views.IndexView.as_view()),
+    path('', views.IndexView.as_view(), name = 'index'),
+    #login view. Commment
+	#path('<slug:slug>/',views.LoginView.as_view(), name='login'),
+	#path('login/',views.LoginView.as_view(), name='login'),
+
+    #ejemplo: /pi/2
+    path('<int:pk>/',views.HomeView.as_view(), name='home'),
+    path('accounts/', include('django.contrib.auth.urls'), name='login'),
+
+]
---
+++ pi/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+from django.http import HttpResponse	#n
+from django.template import loader	#n
+from django.http import Http404	#n
+from .models import Pizza, Empleado
+
+from django.views import generic
+from django.urls import reverse
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
+# Create your views here.
+
+class IndexView(generic.ListView):
+	template_name='index.html'
+	context_object_name='pizza_list'
+
+	def get_queryset(self):
+		return Pizza.objects.all()
+
+class HomeView(generic.DetailView):
+	model=Pizza
+	template_name='home.html'
+
+#comentar en caso de err
+
+class LoginView(generic.DetailView):
+	model=Empleado
+	template_name='registration/login.html'
+
+#...
+
+#nuevo
+def home(request, pizza_id):
+	pizza=get_object_or_404(Pizza, pk=pizza_id)
+	#return HttpResponse("estas mirando la pizza: %s." % pizza_id)
+	return render(request,'home.html', {'pizza':pizza})
+
+
+#comentar en caso de error
+"""
+def login(request, string):
+	return render(request,'pi/login.html')
+#......
+"""
